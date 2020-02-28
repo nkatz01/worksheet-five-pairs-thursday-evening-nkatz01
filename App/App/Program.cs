6bb182c8e8b7009a9efb6a481dcc8972d89398fc
unknown <Nuchem@CS00041648.ad.ucl.ac.uk>
2020-02-28 14:46:10 +0000

    closer than further

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -8,11 +8,11 @@ namespace App
         {
 
 
-            Console.WriteLine(Encipher("z", 0));
-            Console.WriteLine(Encipher("Z", 0));
-           // Console.WriteLine();
-           // Console.WriteLine(Encipher("a", 0));
-           //Console.WriteLine(Encipher("A", 0));
+            Console.WriteLine(Encipher("A",13));
+            Console.WriteLine(Encipher("a",13));
+            Console.WriteLine( );
+         //   Console.WriteLine(Encipher("M", 13));
+         //  Console.WriteLine(Encipher("m", 13));
 
 
 
@@ -21,21 +21,21 @@ namespace App
         }
     
 
-    static char Helper(char[] c, int p)
+    static char Helper(char c1, int p)
     {
 
-            if (p > 25)
+            if (p < 1)
                 throw new ArgumentOutOfRangeException("Only postition between 1 - 26 are allowed");
-       // int p = 26;
+            // int p = 26;
             //  char c1 = 'z';
-            char c1 = (char)c.GetValue(0);
-        //char c2 = 'n';
-        //Console.WriteLine((int)c1);
-        //Console.WriteLine((int)c2);
-        //Console.WriteLine();
-        //Console.WriteLine((char)(109 % 255));
-        //Console.WriteLine((char)(110 % 255));
-        //Console.WriteLine();
+            //    char c1 = (char)c.GetValue(0);
+            //char c2 = 'n';
+            //Console.WriteLine((int)c1);
+            //Console.WriteLine((int)c2);
+            //Console.WriteLine();
+            //Console.WriteLine((char)(109 % 255));
+            //Console.WriteLine((char)(110 % 255));
+            //Console.WriteLine();
 
             //Console.WriteLine((char)(122));
             //Console.WriteLine((char)(122 % 255));
@@ -44,32 +44,123 @@ namespace App
             //Console.WriteLine((char)(90 % 255));
             //Console.WriteLine();
 
+
             if (c1 >= (int)'a' && c1 <= (int)'z' || c1 >= (int)'A' && c1 <= (int)'Z')
-        {
-
-            if (c1 <= 'm' && c1 >= 'a' || c1 <= 'M')
             {
-               // Console.WriteLine("below m or M");
-            c1 =   (char)((int)c1 + p);
-            }
 
-            else
-            {
-           c1 =   (char)((c1 + p) % 255 - 26);
-            }
 
-          
+                Console.WriteLine("below m or M");
+
+                if (p <= 13)//how to prevent letters from n onwards from falling off at 13
+                {
+
+
+                    int positionOfc1InABC = char.IsUpper(c1) ? c1 - 64 : c1 - 96;
+                    Console.WriteLine(positionOfc1InABC);
+                    if (positionOfc1InABC <= 13 || (positionOfc1InABC > 13 && p<positionOfc1InABC) ) { 
+                        c1 = (char)(c1-1 + (((positionOfc1InABC) + p) % 27));
+                  //  Console.WriteLine((int)c1);
+                    }
+
+                    else
+                        c1 = (char)(c1 + (((positionOfc1InABC) + p) % 26));
+
+                }
+
+                else
+                {
+                    //Console.WriteLine("above m or M");
+
+
+
+
+                    c1 = (char)(c1 - (13 - ((13 + p) % 26)));
+                }
+
+            }
+            return c1;
 
         }
 
-            return c1;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //    if (c1 >= (int)'a' && c1 <= (int)'z' || c1 >= (int)'A' && c1 <= (int)'Z')
+            //{
+
+            //    if ((c1 <= 'm' && c1 >= 'a') || c1 <= 'M')
+            //        {    
+            //           Console.WriteLine("below m or M");
+
+            //            if (p <= 13) { 
+
+            //              c1 =   (char)(c1 +  p  );}
+            //            else
+            //                // c1 = (char)(c1 - (p-1)) ;
+            //            c1 = (char)(c1 - (13-((13 + p) % 26)));
+            //        }
+
+            //    else
+            //    {
+            //            //Console.WriteLine("above m or M");
+
+
+            //            if (p < 13)
+            //            {
+            //                c1 = (char)(c1 + (p));
+            //            }
+            //            else
+            //            {
+            //                c1 = (char)(c1 - (13 - ((13 + p) % 26)));
+            //            }
+
+            //        }
+
+
+
+            //}
+
+    //        return c1;
+    //}
 
     static string Encipher(string s, int p)
     {
             if (s.Length == 1)
 
-                return Helper(s.ToCharArray(), p).ToString();
+                return Helper(s[0], p).ToString();
             else return s; 
 
     }  
